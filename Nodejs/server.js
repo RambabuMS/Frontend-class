e651c0cb4e2f6fb1dc149fa32ab33df76c48daf3
Rambabu M <raambabu96@gmail.com>
2026-04-07 09:43:46 +0530

    callback, promise types

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -1,7 +1,8 @@
 const userName = require("./user");
 const os = require("os");
-const fs = require("fs");
+// const fs = require("fs");
 const http = require("http");
+const fs = require("fs/promises");
 
 // console.log("Welcome to Nodejs", userName);
 // console.log("Version", os.version());
@@ -44,11 +45,120 @@ const http = require("http");
 // });
 const PORT = 4000;
 
-const server = http.createServer((req, res) => {
-  res.end("Welcome to Nodejs Server");
-});
-console.log("testing nodemon");
+// const server = http.createServer((req, res) => {
+//   // res.end("Welcome to Nodejs Server");
+//   if (req.url === "/") {
+//     res.end("Home Page");
+//   } else if (req.url === "/about") {
+//     res.end("About Page");
+//   } else {
+//     res.end("404 Not Found");
+//   }
+// });
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     const data = fs.readFileSync("./home.html");
+//     res.end(data);
+//   }
+// });
 
-server.listen(PORT, (err) => {
-  console.log(`server started at ${PORT}`);
-});
+// json response
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/api") {
+//     const data = {
+//       name: "Nirmal",
+//       role: "Developer",
+//     };
+
+//     res.setHeader("Content-Type", "application/json");
+//     res.end(JSON.stringify(data));
+//   }
+// });
+
+// console.log("Start");
+// const data = fs.readFileSync("./home.html", "utf-8");
+// console.log(data);
+// console.log("End");
+
+// console.log("Start");
+// fs.readFile("./home.html", "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+// console.log("End");
+// server.listen(PORT, (err) => {
+//   console.log(`server started at ${PORT}`);
+// });
+
+// console.log("start");
+// fs.readFile("./home.html", "utf-8").then((data) => {
+//   console.log(data);
+// });
+// console.log("End");
+
+//callback hell
+// setTimeout(() => {
+//   console.log("Step 1");
+
+//   setTimeout(() => {
+//     console.log("Step 2");
+
+//     setTimeout(() => {
+//       console.log("Step 3");
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+function step1() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Step 1");
+      resolve();
+    }, 1000);
+  });
+}
+function step2() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Step 2");
+      resolve();
+    }, 1000);
+  });
+}
+function step3() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Step 3");
+      resolve();
+    }, 1000);
+  });
+}
+
+// Promise.all([Promise.reject("A"), Promise.reject("B")])
+//   .then(console.log)
+//   .catch(console.log);
+
+// Promise.allSettled([Promise.reject("A"), Promise.reject("Error")]).then(
+//   console.log,
+// );
+
+// Promise.race([
+//   new Promise((res) => setTimeout(() => res("slow"), 3000)),
+//   new Promise((res) => setTimeout(() => res("fast"), 2000)),
+//   new Promise((res, rej) => setTimeout(() => rej("fast1"), 2000)),
+// ]).then(console.log);
+
+Promise.any([
+  Promise.reject("Error"),
+  Promise.resolve("Success"),
+  Promise.resolve("Success2"),
+]).then(console.log);
+
+// step1().then(step2).then(step3).catch(console.log);
+
+// async function run() {
+//   await step1();
+//   await step2();
+//   await step3();
+// }
+// run();
